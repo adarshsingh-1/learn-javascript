b051044dc52f0e8ecc1be19e5329e9a10b008686
Adarsh Singh <112319359+adarshsingh-1@users.noreply.github.com>
2025-04-09 16:30:56 +0000

    datatypes in js

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -1,3 +1,27 @@
 "use strict" //treat all js code as newer version
 
-alert( 3 + 3 )
+//alert( 3 + 3 ) //we are using node js not browser
+
+console.log(3 + 3) //6;
+
+let name = "hitesh"
+let age = 23
+let isLoggedIn = false
+
+
+//primituve data types
+
+//number range  = 2^53
+//bigint used for large numbers in trading stock or reddit
+//string => ""
+//boolean => true/false
+//null => standalone value or empty value
+//undefined => variable is declared but not assigned
+//symbol => unique identifier
+
+
+
+console.log(typeof name) //string
+console.log(typeof age) //number
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined
