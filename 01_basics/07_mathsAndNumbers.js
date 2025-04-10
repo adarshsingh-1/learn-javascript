2e1dd92379eecbe65251194b310a998da53e885e
Adarsh Singh <112319359+adarshsingh-1@users.noreply.github.com>
2025-04-10 17:12:07 +0000

    maths and numbers in js

---
+++ 01_basics/07_mathsAndNumbers.js
@@ -0,0 +1,51 @@
+const score = 400
+//console.log(score); //400
+
+
+const balance = new Number(100)
+//console.log(balance); //Number {100}
+
+//console.log(balance.toString().length); //100
+//console.log(typeof balance); //object
+//console.log(balance.toFixed(2)); //100.00
+
+
+
+
+
+const num1 = 123.8565
+//console.log(num1.toPrecision(3)); //124
+
+
+const hundreds = 1000000
+//console.log(hundreds.toLocaleString('en-IN')); //10,00,000
+
+
+// ********************************** Maths ************************************
+
+
+console.log(Math); //3.141592653589793
+// console.log(Math.abs(-4)); //4
+// console.log(Math.round(4.7)); //4.3
+// console.log(Math.ceil(4.2)); //4.3
+// console.log(Math.floor(4.2)); //4.3
+
+// console.log(Math.sqrt(16)); //4
+// console.log(Math.pow(2, 3)); //8
+// console.log(Math.max(2, 3, 4, 5)); //5
+// console.log(Math.min(2, 3, 4, 5)); //2
+
+
+//console.log(Math.random()); //0.123456789
+//console.log((Math.random()*10)+1); //0.123456789
+
+
+const min = 10
+const max = 20
+//console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+//+1 is added to make max value inclusive 
+
+//console.log(Math.random() * (max - min) + min);
+//This gives a random decimal number in the range [min, max) — inclusive of min, exclusive of max.
+
+
