1cf9d00132c865a768f209895f62fab9a2fa068b
Adarsh Singh <112319359+adarshsingh-1@users.noreply.github.com>
2025-04-11 16:58:19 +0000

    objects in js

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,56 @@
+//singleton - Ek hi object banega, baar-baar use hoga
+//object.create //comstructor metho
+//object literal
+
+
+const mySym = Symbol("key1")
+
+
+const user = {
+    //key: value pair 
+    name: "adarsh",
+    "full name": "adarsh kumar",
+    [mySym] : "myKey1",
+    age: 23,
+    email:"adarsh@gmail.com",
+    location:"kolkata",   
+    isActive: true,
+    hobbies: ["reading", "gaming"],
+    lastLoginDays:["monday", "tuesday", "wednesday"], 
+}
+
+
+
+//accessing object properties
+//dot notation
+//console.log(user.name); //adarsh
+//console.log(user.age); //23
+ //adarsh
+
+
+//bracket notation
+//console.log(user["name"]); //adarsh
+//console.log(user["age"]); //23
+//console.log(user["full name"]); //adarsh kumar
+//console.log(user[mySym]);
+// console.log(typeof user.mySym);
+
+//bracket notation is useful when key is dynamic
+
+
+user.email = "raju@gmail.com"
+//console.log(user["email"]);
+
+// Object.freeze(user) //object ko freeze kar do, ab koi change nahi hoga
+user.email = "adarshsingh@gmail.com"
+//console.log(user["email"]);
+
+
+user.greeting = function(){
+    console.log("Hello JS user");
+    
+}
+
+console.log(user.greeting); 
+
+// In JavaScript, if a function does not explicitly return a value, it returns `undefined` by default.
