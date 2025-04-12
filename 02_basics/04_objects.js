7dbdff3c250639a8070c02214a0d8b5a3826306e
Adarsh Singh <112319359+adarshsingh-1@users.noreply.github.com>
2025-04-12 16:02:16 +0000

    object deconstructing in js

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -34,7 +34,7 @@ const obj3 = {5:"a", 6: "b"}
 // const obj3 = {obj1, obj2}
 //const obj4 = Object.assign({}, obj1, obj2, obj3)
 const obj4 = {...obj1,...obj2,...obj3}
-console.log(obj4);
+// console.log(obj4);
 
 
 const adminUser = [
@@ -55,15 +55,48 @@ const adminUser = [
 //  console.log(adminUser[0].email);
 
 
-console.log(tinderUser);
+//console.log(tinderUser);
 
-console.log(Object.keys(tinderUser)); // gives the keys of the object
-console.log(Object.values(tinderUser)); // gives the values of the object
-console.log(Object.entries(tinderUser)); // gives the key value pair of the object
-console.log(tinderUser.hasOwnProperty('id')); // checks if the object has the property
+//console.log(Object.keys(tinderUser)); // gives the keys of the object
+//console.log(Object.values(tinderUser)); // gives the values of the object
+//console.log(Object.entries(tinderUser)); // gives the key value pair of the object
+//console.log(tinderUser.hasOwnProperty('id')); // checks if the object has the property
 
 
-console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+//console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+
+// ************************************* Object Deconstructing **************************************
+
+
+// Object destructuring JavaScript ka ek feature hai jo humein kisi object ke properties ko easily extract karke individual variables mein assign karne ka tareeqa deta hai. Ye short syntax hai jo code ko clean aur readable banata hai.
+
+
+const student = {
+    fullName: "Adarsh",
+    Class: 10,
+    course: "CSE"
+  };
+  
+  // Normal way:
+  const fullName = student.fullName;
+  const Class = student.Class;
+  
+//Rename karna destructuring mein:
+const { fullName: studentName, Class: studentAge } = student;
+
+// console.log(studentName); // Adarsh
+// console.log(studentAge);  // 10
+
+  //  Destructuring way:
+  const { name, age, course } = student;
+  
+  console.log(fullName);  // Adarsh
+  console.log(Class);   // 10
+  console.log(course); // CSE
+
+
+
 
 
 
