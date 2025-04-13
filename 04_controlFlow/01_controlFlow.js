f45b15d4a942c1ad9b77938cb872db7b82c00268
Adarsh Singh <112319359+adarshsingh-1@users.noreply.github.com>
2025-04-13 12:11:02 +0000

    control flow in js

---
+++ 04_controlFlow/01_controlFlow.js
@@ -0,0 +1,46 @@
+//if statement
+
+//if condition - > true andar ka code execite karenge
+// if(2 != "2"){
+//     console.log("done");
+    
+// }
+// = -> assigment operator
+// == -> equality
+
+// <, >, <=, >=, ==,!=, ===, !==
+
+
+// const isUserLoggedIn = true;
+// const temperature = 41;
+
+// if(temperature === 41){
+//     console.log("less than 50");
+// }else{
+//     console.log("temperature is greater than 50");
+    
+// }
+
+// console.log("Execute");
+
+
+// const score = 200;
+// if(score>100){
+//     const power = "fly"
+//     console.log(`user has power of ${power}`);
+// }
+// console.log(`user has power of ${power}`); power can't be accessed outside the block
+
+
+//shorthand if statement
+const balance  = 1000;
+//  if(balance>500) console.log("test");
+
+
+
+
+ 
+
+
+
+
