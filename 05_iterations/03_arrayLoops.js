16c0b0b75ed1f160caef713e36651e3ce0be577c
Adarsh Singh <112319359+adarshsingh-1@users.noreply.github.com>
2025-04-13 17:16:18 +0000

    for of in js

---
+++ 05_iterations/03_arrayLoops.js
@@ -0,0 +1,76 @@
+//for of loop
+
+const numbers = [1, 2, 3, 4, 5];
+for (const number of numbers) {
+  //console.log(number);
+}
+
+ //for of loop with strings
+const str = "Hello";
+for (const char of str) {
+  //console.log(char);
+}
+
+
+
+
+//Maps
+//map.size – returns total number of entries.
+//map.keys() – returns all keys.
+//map.values() – returns all values.
+//map.entries() – returns all key-value pairs.
+//create a map
+const map = new Map()
+map.set('IN', "India")  
+map.set('US', "United States")
+map.set('UK', "United Kingdom")
+map.set('AU', "Australia")
+
+
+//getting values from map
+// console.log(map.get('IN')); 
+// console.log(map.get('US'));      
+// console.log(map);
+
+//uisng for of loop to iterate over map
+for (const [key, value] of map) {
+  //console.log(key, ':- ', value);
+}
+
+
+
+
+
+//for of loop with objects
+const person = {
+  name: "John",
+  age: 30,
+  city: "New York",
+};
+for (const key of Object.keys(person)) {
+  console.log(key, person[key]);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
