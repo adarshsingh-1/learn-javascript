4b086bb99c0d911702ea4e7314cf9b9fcd79a36a
Adarsh Singh <112319359+adarshsingh-1@users.noreply.github.com>
2025-04-13 15:23:24 +0000

    do while loops in js

---
+++ 05_iterations/02_doWhile.js
@@ -0,0 +1,28 @@
+// while (condition) {
+    // code block
+    // console.log("user can do transaction");
+    // break;  
+// }
+
+let ind = 0
+while (ind<10) {
+    //console.log(`Current number is ${ind}`);
+    ind++;
+}
+
+
+let myArray = ['flash ', 'batman', 'superman', 'ironman', 'thor'];
+let index = 0;
+while (index < myArray.length) {
+    //console.log(`Value is ${myArray[index]}`);
+    index++;
+}
+
+
+
+let score = 1;
+do {
+    console.log(`score is ${score}`);
+    score++;
+    
+} while (score<=10);
