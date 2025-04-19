6d2ed311f95d7a1d522c2e5d875665b98c6f606b
Adarsh Singh <112319359+adarshsingh-1@users.noreply.github.com>
2025-04-19 17:12:48 +0000

    promises in js

---
+++ 09_advance_one/promises.js
@@ -0,0 +1,95 @@
+// const promise1  = new Promise(function(resolve, reject) {
+//     //do any task
+//     //BD calls , cryptography, file system, network calls
+//     setTimeout(function() {
+//     //    console.log('Async Task  completed');
+//        resolve()
+//     }, 1000);
+// })
+
+// promise1.then(function(){
+//     // console.log("Promise consumed");
+    
+// })
+
+
+// // Promise chaining
+
+// new promise2(function(resolve, reject) {
+//     setTimeout(function() {
+//         // console.log('Async Task 2 completed');
+//         resolve()
+//     }
+//     , 2000);
+// }).then(function(){
+//     // console.log("Promise 2 consumed");
+// })
+
+
+const promise3 = new Promise(function(resolve, reject) {
+    setTimeout(function(){
+        resolve({usename:"john", email:"john@hmail.com"})
+    },1000)
+
+})
+
+promise3.then(function(user){
+    console.log("User data: ", user);
+    
+})
+
+
+
+
+
+const promise4 = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        let error = true;
+        if(!error){
+            resolve({username:"john", password: "1234"})
+        }else{
+            reject("Error: Something went wrong")
+        }
+    },1000)
+})
+
+
+
+promise4.then((user) => {
+    console.log("User data: ", user);
+    return user.username
+}).then((username)=>{
+    console.log("Username: ", username);
+    
+}).catch(function(error){
+    console.log(error);
+}).finally(() => console.log("The promise is either resolved or rejected"))
+
+
+
+
+
+
+const promise5 = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        let error = true;
+        if(!error){
+            resolve({username:"raju", password: "raju@gmail.com"})
+        }else{
+            reject("ERROR: Something went wrong RAJU")
+        }
+    },1000)
+})    
+
+
+async function consumerPromise5(){
+    try{
+        const response  = await promise5
+        console.log(response);
+    }catch{
+        console.log(error);
+    }
+    
+}
+
+consumerPromiseFive()
