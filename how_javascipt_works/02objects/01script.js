20c86edae0c209dc27df04c1d356be6f12d05b70
Adarsh Singh <112319359+adarshsingh-1@users.noreply.github.com>
2025-07-21 14:07:40 +0000

    objects in js

--- how_javascipt_works/02objects/01script.js
+++ how_javascipt_works/02objects/01script.js
@@ -1,26 +1,30 @@
-const person = {
-    fname : "Adarsh",
-    lname: "Singh",
-    contact: 7645678902,
-    getname: function(){
-        console.log("Raj");
-    },
-}
+// const person = {
+//     fname : "Adarsh",
+//     lname: "Singh",
+//     contact: 7645678902,
+//     getname: function(){
+//         console.log("Raj");
+//     },
+//     getContact(){},
+// }
 
 
-const p2 = {
-    fname:"Jane",
-    lanme: "Doe",
-    contact: 1234567890,
-    getname: function(){
-        console.log("Jane");
-    },
+// const p2 = {
+//     fname:"Jane",
+//     lanme: "Doe",
+//     contact: 1234567890,
+//     getname: function(){
+//         console.log("Jane");
+//     },
 
 
-}
+// }
+
+//normal function -> camelCase
+// getAge(), addNums(), printThis()
 
 
-// constructor function -> Pascal Case
+// constructor function -> PascalCase
 // Example:
 
 function Person(fname, lname, contact) {
@@ -32,4 +36,16 @@ function Person(fname, lname, contact) {
         console.log(this.fname);
     }
 
+    this.getContact = function(){
+        console.log(this.contact);
+    }
+
 }
+
+const p1 = new Person("Adarsh", "Singh", 7645678902);
+const p2 = new Person("Jane", "Doe", 1234567890);
+const p3 = new Person("John", "Smith", 9876543210);
+
+console.log(p1);
+console.log(p2);
+console.log(p3);
